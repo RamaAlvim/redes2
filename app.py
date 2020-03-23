df5e18b955279e1d89615757b4eeb5d6a65fcf62
rama.alvim <rama.alvim@4mti.com.br>
2020-03-23 19:07:53 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# import the Flask class from the flask module
+from flask import Flask, render_template, redirect, url_for, request
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+
+# create the application object
+app = Flask(__name__)
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return login()
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
